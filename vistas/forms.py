774690e343b3e69c166080df901c67e5b706d1f6
Rodrigo Giménez-Mermot <rhigimenez@gmail.com>
2022-07-05 21:44:34 -0300

    Version 3.1

--- vistas/forms.py
+++ vistas/forms.py
@@ -2,8 +2,8 @@ from django import forms
 
 class crear_producto(forms.Form):
     productoCia =  forms.CharField()
-    productoCodigo = forms.IntegerField()
-    productoDescripcion = forms.IntegerField()
+    productoCodigo = forms.CharField()
+    productoDescripcion = forms.CharField()
     productoCantidad = forms.IntegerField()
     productoCosto = forms.IntegerField()
     
