a3dcc44293e5675b8da0860baa845b588c4b00b4
iSanty <santiago.navalon@hotmail.com>
2022-07-17 11:39:43 -0300

    Arregle variables y nombres de clases y forms

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Proyecto Final de Santiago Navalon y Rodrigo Gimenez
 - Funcionan todos los campos, esta todos los links deberian servir menos los del login
-- Por logica, en un alta de producto en un sistema de gestion de stock, no se da de alta con cantidad ya que es solo un objeto. Pero lo pusimos para la investigacion de como subar a la base en vez de solo escribir.
+- Por logica, en un alta de producto en un sistema de gestion de stock
 
--- vistas/forms.py
+++ vistas/forms.py
@@ -5,7 +5,8 @@ class FormCrearProducto(forms.Form): #estos nombres tienen que ser por ejemplo: 
     producto_cia =  forms.CharField(max_length=30)
     producto_codigo = forms.CharField()
     producto_descripcion = forms.CharField()
-    producto_costo = forms.IntegerField() #aca pondria required=False
+    producto_costo = forms.IntegerField(required=False)
+    producto_fecha = forms.DateField(required=False)#aca pondria required=False
     
 class FormBusquedaProducto(forms.Form):
     producto_codigo = forms.CharField(max_length=30)
